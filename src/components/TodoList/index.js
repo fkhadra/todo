df37c52bddd378126486a0c9d0291f200dafdf4f
Fadi Khadra <fdkhadra@gmail.com>
2018-01-13 22:27:16 +0100

    tweaking exit animation

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
-import { css } from 'glamor';
+import { css, keyframes } from 'glamor';
+import { TransitionGroup, Transition } from 'react-transition-group';
 
 import Todo from './Todo';
 import TodoInput from './TodoInput';
+
+const animations = {
+  slideInLeft: keyframes({
+    from: {
+      transform: 'translate3d(-100%, 0, 0)',
+      opacity: 0
+    },
+    to: {
+      opacity: 1,
+      transform: 'none'
+    }
+  }),
+  zoomOut: keyframes({
+    from: {
+      opacity: 1
+    },
+
+    '50%': {
+      opacity: 0,
+      transform: 'scale3d(.3, .3, .3)'
+    },
+
+    to: {
+      opacity: 0
+    }
+  }),
+
+};
 
 const styles = {
   list: css({
     listStyle: 'none',
     padding: 0
+  }),
+  enter: css({
+    animationFillMode: 'both',
+    animationDuration: '0.75s',
+    animationName: animations.slideInLeft
+  }),
+  exit: height => css({
+    animationFillMode: 'both',
+    animationDuration: '0.75s',
+    animationName: animations.zoomOut,
+    transition: 'max-height 0.75s, opacity 0.75s',
+    maxHeight: height,
+    overflow: 'hidden',
   })
 };
 export default class TodoList extends Component {
@@ -21,18 +63,45 @@ export default class TodoList extends Component {
     });
   }
 
+  ext = el => {
+    const timing = '0.75s';
+    const height = el.offsetHeight + 'px';
+   
+   
+    el.classList.add(styles.exit(height));
+    // Begin transition
+    // var endSlideDown = function() {
+    //   el.style.removeProperty('-webkit-transition');
+    //   el.style.removeProperty('transition');
+    //   el.removeEventListener(transitionEvent('end'), endSlideDown);
+    // };
+    requestAnimationFrame(function() {
+      el.style.maxHeight = '0';
+      el.style.opacity = '0';
+    });
+  }
+
   renderTodos() {
     const { toggleDone, updateTodo, removeTodo } = this.props.todoStore;
 
     return this.state.todos.map(todo => (
-      <li key={todo.id}>
-        <Todo
-          todo={todo}
-          toggleDone={toggleDone}
-          updateTodo={updateTodo}
-          removeTodo={removeTodo}
-        />
-      </li>
+      <Transition 
+      key={todo.id}
+      timeout={750}
+      onEnter={node => node.classList.add(styles.enter)}
+      onEntered={node => node.classList.remove(styles.enter)}
+      onExit={this.ext}
+      
+      >
+        <li>
+          <Todo
+            todo={todo}
+            toggleDone={toggleDone}
+            updateTodo={updateTodo}
+            removeTodo={removeTodo}
+          />
+        </li>
+      </Transition>
     ));
   }
 
@@ -44,7 +113,9 @@ export default class TodoList extends Component {
         <h2>{title}</h2>
         <div>
           <TodoInput addTodo={todoStore.addTodo} />
-          <ul {...styles.list}>{this.renderTodos()}</ul>
+          <TransitionGroup {...styles.list} component="ul">
+            {this.renderTodos()}
+          </TransitionGroup>
         </div>
       </section>
     );
