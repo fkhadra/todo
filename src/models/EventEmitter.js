77a1d650d6d11c25b42ff9684b902253e7a49233
Fadi Khadra <fdkhadra@gmail.com>
2018-01-31 14:58:09 +0100

    Add flow typing

--- src/models/EventEmitter.js
+++ src/models/EventEmitter.js
@@ -1,21 +1,27 @@
+/* @flow */
+
+type func = () => void;
+
 export default class EventEmitter {
-  listeners = new Map();
+  listeners: Map<string, Array<func>>= new Map();
   events = {
     ON_CHANGE: 'on_change'
   };
   
-  onChange(cb) {
+  onChange(cb: func) {
     return this.on(this.events.ON_CHANGE, cb);
   }
 
-  on(event, cb) {
+  on(event: string, cb: func) {
     this.listeners.has(event) || this.listeners.set(event, []);
+    // $FlowFixMe
     this.listeners.get(event).push(cb);
     return this;
   }
 
-  dispatch(event, ...payload) {
+  dispatch(event: string, ...payload:any): void {
     if (this.listeners.has(event)) {
+      // $FlowFixMe
       this.listeners
         .get(event)
         .forEach(cb => setTimeout(() => cb.call(this, ...payload), 0));
--- src/models/Todo.js
+++ src/models/Todo.js
@@ -1,29 +1,47 @@
-import { uuid } from 'src/utils';
+/* @flow */
+
+type todo = {
+  listId: string,
+  value: string,
+  id: string,
+  done: boolean,
+  createdBy: string | null,
+  modifiedBy: string | null,
+  createdAt: number,
+  updatedAt: number | null
+};
 
 class Todo {
+  listId: string;
+  value: string;
+  id: string;
+  done: boolean;
+  createdBy: string | null;
+  modifiedBy: string | null;
+  createdAt: number;
+  updatedAt: number | null;
+  
   constructor({
     listId,
-    id = null,
-    value = '',
-    done = false,
-    createdBy = null,
-    ModifiedBy = null,
-    createdAt = null,
-    updatedAt = null
-  }) {
-    const now = Date.now();
-
-    this.id = id || uuid();
+    value,
+    id,
+    done,
+    createdBy,
+    modifiedBy,
+    createdAt,
+    updatedAt
+  }: todo) {
+    this.id = id;
     this.listId = listId;
     this.value = value;
     this.done = done;
     this.createdBy = createdBy;
-    this.ModifiedBy = ModifiedBy;
-    this.createdAt = createdAt || now;
+    this.modifiedBy = modifiedBy;
+    this.createdAt = createdAt;
     this.updatedAt = updatedAt;
   }
 
-  update(payload) {
+  update(payload: todo): void {
     Object.assign(this, {
       ...payload,
       updatedAt: Date.now()
