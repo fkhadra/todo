79e4b39372f3bd8ee50bd4f056f363406f79785f
Fadi Khadra <fdkhadra@gmail.com>
2018-05-25 15:35:05 +0200

    Unmount transition when end they end

--- src/components/App/LoadingScreen.js
+++ src/components/App/LoadingScreen.js
@@ -1,80 +1,69 @@
 import React, { Fragment } from 'react';
 import { css } from 'glamor';
 
-import { Spinner } from 'src/components/Misc';
 import logo from 'src/assets/logo.svg';
+import { withTransition } from 'src/components/Misc';
 
-const common = {
-  position: 'fixed',
-  top: 0,
-  width: '51%',
-  height: '100%',
-  transition: 'transform 1s',
-  zIndex: 10
-};
-
-const a = css.keyframes({
-  from: { transform: 'rotate(0deg)' },
-  to: { transform: 'rotate(360deg)' }
-});
 
 const styles = {
+  common: {
+    position: 'fixed',
+    top: 0,
+    width: '51%',
+    height: '100%',
+    transition: 'transform 1s',
+    zIndex: 10
+  },
+  rotateAnimation: css.keyframes({
+    from: { transform: 'rotate(0deg)' },
+    to: { transform: 'rotate(360deg)' }
+  }),
   left: isLoading =>
     css({
-      ...common,
+      ...styles.common,
       left: 0,
       transform: isLoading ? 'translateX(0)' : 'translateX(-100%)',
       background: 'linear-gradient(to right, #000000, #434343)'
     }),
   right: isLoading =>
     css({
-      ...common,
+      ...styles.common,
       right: 0,
       transform: isLoading ? 'translateX(0)' : 'translateX(100%)',
       background: 'linear-gradient(to right, #434343, #000000)'
+    }),
+  logo: isLoading =>
+    css({
+      position: 'fixed',
+      left: '50%',
+      top: '50%',
+      transform: 'translate(-50%, -50%)',
+      zIndex: 11,
+      opacity: isLoading ? 1 : 0,
+      transition: 'opacity 0.6s',
+      '& img': {
+        width: '200px',
+        animation: `${styles.rotateAnimation} 6s linear infinite`
+      }
     })
 };
 
-export default class extends React.Component {
-  
-  handleTransitonEnd = e => {
-    console.log(e.target.dataset.trans)
-  };
+const Panel = withTransition(({ isLoading, side, unmount }) => (
+  <div {...styles[side](isLoading)} onTransitionEnd={unmount} />
+));
+const Logo = withTransition(({ isLoading, unmount }) => (
+  <div {...styles.logo(isLoading)} onTransitionEnd={unmount}>
+    <img src={logo} alt="logo" />
+  </div>
+));
 
-  render(){
-    const { isLoading, children } = this.props;
-   
-    
-    return (
-      <Fragment>
-        {/* {isLoading ? <Spinner /> : children} */}
-        <div {...styles.left(isLoading)}  data-trans="left" onTransitionEnd={this.handleTransitonEnd}/>
-        <div {...styles.right(isLoading)} data-trans="right" />
-        {/* <div
-          {...css({
-            position: 'fixed',
-            left: '50%',
-            top: '50%',
-            transform: 'translate(-50%, -50%)',
-            zIndex: 11,
-            opacity: isLoading ? 1 : 0,
-            transition: 'opacity 0.6s'
-          })}
-          data-trans="logo"
-        >
-          <img
-            src={logo}
-            {...css({
-              width: '200px',
-              animation: `${a} 6s linear infinite`,
-              pointerEvents: 'none'
-            })}
-            alt=""
-          />
-        </div> */}
-        {children}
-      </Fragment>
-    );
-  }
-}
+const LoadingScreen = ({ isLoading, children }) => (
+  <Fragment>
+    <Panel isLoading={isLoading} side="left" />
+    <Panel isLoading={isLoading} side="right" />
+    <Logo isLoading={isLoading} />
+    {children}
+  </Fragment>
+)
 
+export default LoadingScreen;
--- src/components/Misc/index.js
+++ src/components/Misc/index.js
@@ -1,3 +1,4 @@
 export { default as Input } from './Input';
 export { default as Spinner } from './Spinner';
 export { default as Menu } from './Menu';
+export { default as withTransition } from './withTransition';
