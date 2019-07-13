92b6ca8f06ac74bd7a61037eae6a70ee6689a338
Fadi Khadra <fdkhadra@gmail.com>
2019-07-13 09:28:53 +0200

    spinner ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/ts/App';
+import * as serviceWorker from './components/ts/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
