fd72ca911eb49378b538fa536fd7140c50b35139
Fadi Khadra <fdkhadra@gmail.com>
2019-07-15 19:08:58 +0200

    Css transition powa 💥

--- src/components/TodoList/Checkmark.tsx
+++ src/components/TodoList/Checkmark.tsx
@@ -1,41 +1,23 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
-const animations = {
-  dash: keyframes`
-    0%: {
-      stroke-dashoffset: 1000;
-    },
-    100%: {
-      stroke-dashoffset: '0'
-    }
-  `,
-  check: keyframes`
-    0%: {
-      stroke-dashoffset: -100
-    },
-    100%: {
-      stroke-dashoffset: 900
-    }
-  `
-};
-
 const Circle = styled.circle<{ checked: boolean }>`
   fill: none;
   stroke-width: 6;
   stroke-miterlimit: 10;
+  transition: stroke-dashoffset 0.35s ease-in-out, stroke 0.35s;
   stroke: ${props => (props.checked ? '#73AF55' : '#ffffff')};
-  animation: ${animations.dash} 0.9s ease-in-out;
+  stroke-dashoffset: ${props => (props.checked ? 0 : 1000)};
   stroke-dasharray: 1000;
   stroke-dashoffset: 0;
 `;
 
-const Line = styled.polyline`
+const Line = styled.polyline<{ checked: boolean }>`
   fill: none;
   stroke-dasharray: 1000;
-  stroke-dashoffset: -100;
-  animation: ${animations.check} 0.9s 0.35s ease-in-out forwards;
-  stroke: #73af55;
+  stroke-dashoffset: ${props => (props.checked ? 900 : -100)};
+  transition: stroke-dashoffset 0.35s ease-in-out, stroke 0.35s;
+  stroke: ${props => (props.checked ? '#73AF55' : '#ffffff')};
   stroke-width: 6;
   stroke-linecap: round;
   stroke-miterlimit: 10;
@@ -53,7 +35,6 @@ export const Checkmark: React.FC<CheckmarkProps> = ({ checked }) => (
     width="20"
   >
     <Circle checked={checked} cx="65.1" cy="65.1" r="62.1" />
-    {checked && <Line points="100.2,40.2 51.5,88.8 29.8,67.5 " />}
+    <Line checked={checked} points="100.2,40.2 51.5,88.8 29.8,67.5" />
   </svg>
 );
-
