d3e643b2439a5abe77fa295497cf539c7fb1bb4f
Fadi Khadra <fdkhadra@gmail.com>
2019-07-15 12:58:35 +0200

    add icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,4 @@
 export { default as ellipsis } from './ellipsis.svg';
 export { default as deleteIcon } from './delete.svg';
+export { default as editIcon } from './edit.svg';
+export { default as clearIcon } from './clear.svg';
